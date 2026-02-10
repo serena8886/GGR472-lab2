c58afbf6ba87b77bbee86b72e0d313784fef1696
serena8886 <aquashortcake9@gmail.com>
2026-02-10 16:22:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGdzbWl0aCIsImEiOiJja29uNGs1cmYwYnN2MnBwMzM2c
 const map = new mapboxgl.Map({
     container: 'my-map', // map container ID
     style: 'mapbox://styles/serenaxie/cmlgr6u5n002001qw2abl8rke', //style URL
-    center: [-79.39, 43.66], // starting position [lng, lat] Queens Park
-    zoom: 30, // starting zoom
+    ccenter: [-79.39, 43.66],
+    zoom: 14,
+    pitch: 55,
+    bearing: -10,
+    maxZoom: 18
 });
+
 
 //Listen for load event, once map finishes loading, trigger the following functions
 map.on('load', () => {
